aec1f0959a5f35c0e4832b20fbfef2caa8a8a4a0
Roman Aleksandrov <romech95@gmail.com>
2021-04-29 00:59:16 +0200

    Tweak visualisation a bit

--- experiments/visualise_scores.py
+++ experiments/visualise_scores.py
@@ -8,17 +8,23 @@ from utils import layer_density
 def plot_reconstruction_results(result_output: pd.DataFrame,
                                 edges: pd.DataFrame,
                                 methods=('Random', 'IPF', 'DBCM')):
-    densities = edges.groupby('layer_id').apply(layer_density)
-    stats = result_output[result_output.name.isin(methods)]\
+    densities = edges.groupby('layer_id').apply(layer_density).rename('density')
+    stats = result_output\
         .groupby(level=['name', 'layer_id'])\
         .agg('mean')\
         .reset_index()
-    stats = pd.merge(stats, densities, left_on='layer_id', right_index=True)\
+    stats = pd.merge(stats[stats.name.isin(methods)],
+                     densities,
+                     left_on='layer_id',
+                     right_index=True)\
         .rename(columns={'f1': 'F1 score',
                          'density': 'Layer density',
                          'name': 'Method'})
-    sns.jointplot(data=stats, x='Layer density', y='F1 score', hue='Method')
-    plt.savefig('output/f1_layers.svg')
+    sns.jointplot(data=stats,
+                  x='Layer density',
+                  y='F1 score',
+                  hue='Method',
+                  ylim=(-.05, 1.05 * min(1, result_output.f1.max())))
 
 
 def fao_plot_reconstruction():
@@ -28,6 +34,7 @@ def fao_plot_reconstruction():
     edges = load_all_layers()
     result_output = demo_evaluate_multiple_layers(num_seeds=5)
     plot_reconstruction_results(result_output, edges)
+    plt.savefig('output/fao_f1_layers.svg')
 
 
 if __name__ == '__main__':
